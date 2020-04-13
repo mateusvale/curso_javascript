512c0d035f52689a57ea645b5ad7c5cb0079f3e4
Mateus <mateusvalexandre@gmail.com>
2020-04-13 12:09:21 -0300

    finalizando a aula 4

--- js/principal.js
+++ js/principal.js
@@ -15,11 +15,11 @@ var numeroPacientes = pacientes.length;
 for (var i=0 ; i < numeroPacientes; i++){
 
 //Criando a variavel tdPeso utilizando o querySelector da variavel pacientes + classe:
-var tdPeso = pacientes[i].querySelector(".info-peso");      //em vez de document, eu utilizo o o paciente..
+var tdPeso = pacientes[i].querySelector(".info-peso");      //em vez de document, eu utilizo o o pacientes[i]..
 var peso = tdPeso.textContent;                          //com o .textContent eu consigo pegar o valor do conteudo
 
 //Criando a variavel tdPeso utilizando o querySelector da variavel pacientes + classe:
-var tdPeso = pacientes[i].querySelector(".info-peso");      //em vez de document, eu utilizo o o paciente..
+var tdPeso = pacientes[i].querySelector(".info-peso");      //em vez de document, eu utilizo o pacientes[i]..
 var peso = tdPeso.textContent;                          //com o .textContent eu consigo pegar o valor do conteudo
 
 //Criando a variavel tdAltura utilizando o querySelector da variavel paciente + classe:
@@ -63,8 +63,44 @@ else {
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
 //criando um evento a partir do click do botao
-botaoAdicionar.addEventListener("click",function(){
-    console.log("Fui apertado");
+botaoAdicionar.addEventListener("click",function(event){
+    event.preventDefault();
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value; //estou pegando o name de cada input e adicionando o .value - isso fara com que eu consiga pegar o valor do input
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    //para que possamos criar um paciente com esses dados, precisamos criar uma td (relativa ao novo paciente) e 5 novas tds (nome, peso, altura, gordura e imc)
+
+    //criacao de uma tr
+    var pacienteTr = document.createElement("tr");
+
+    //criacao das 5 tds:
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    //colocando os valores nas tds:
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    //precisamos colocar as tds criadas dentro da tr. Logo precisamos utilizar os seguintes codigos:
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    //como iremos entao colocar essa tr na tabela? Iremos tbm utilizar o append child:
+    var tabela = document.querySelector("#tabela-pacientes"); //pegamos o tdbody onde tem a tabela contendo todos os tr`s
+    tabela.appendChild(pacienteTr);
+
 });
 
 
