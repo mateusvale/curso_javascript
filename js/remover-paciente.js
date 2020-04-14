6e18075a0498e93c49b570c242d1284081d9f465
Mateus <mateusvalexandre@gmail.com>
2020-04-14 16:34:58 -0300

    acabando a aula 7

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,11 +1,22 @@
-//iremos precisar selecionar todos os pacientes:
 var pacientes = document.querySelectorAll(".paciente");
 
-//fazer um lopp para pegar todos os pacientes:
-pacientes.forEach(function(paciente){
-    //evento de listener de mouse - duplo click:
-    paciente.addEventListener("dblclick", function(){
-        //remocao de tag
-        this.remove();
-    });
+//iremos precisar pegar a tabela
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+
+    event.target.parentNode.classList.add("fadeOut");   //iremos criar a classe no css
+
+    setTimeout(function(){                      //iremos criar essa funcao para o apagamento ocorrer mais lentamente
+
+        var alvoEvento = event.target;          // com o .target, iremos conseguir pegar a tag q apertarmos
+        var paiDoAlvo = alvoEvento.parentNode;  // nao queremos apagar a tag do target, e sim o pai, que eh a linha
+        paiDoAlvo.remove();                     // logo iremos apagar a linha da tabela. 
+
+    }, 500);
+
+    
+
+    //podemos remover a linhas utilizando apenas uma linha de codigo:
+    //event.target.parentNode.remove();
 })
